638379290a384ec2bf8dc9ba58e96b72bc98fa9d
KrishJoshi <tuffleo@gmail.com>
2019-04-10 14:51:42 +0100

    headless

---
+++ headless/lambda.js
@@ -0,0 +1,15 @@
+exports.handler = function(event, context, callback) {
+    (async () => {
+    const puppeteerLambda = require('puppeteer-lambda');
+    const browser = await puppeteerLambda.getBrowser({
+    headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    const image = await page.screenshot({path: 'example.png'});
+ 
+     callback(null, image);
+
+    await browser.close(); 
+})();
+}
